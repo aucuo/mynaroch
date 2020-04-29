0b62c7801e3b998bce345786c5fa35c6f17c7ca7
Jahor Sykaviec <jahor.sykaviec@gmail.com>
2020-04-29 14:02:36 +0300

    Небольшие изменения в меню Начал работу по адаптации меню под мобильные устройства.

--- menu.php
+++ menu.php
@@ -1,12 +1,12 @@
 <!-- <div class="preloader"><div class="spinner"></div></div> -->
 
-<menu class="mobile">
+<menu class="">
 <div class="mobileBG"></div>
+<div id="bWrapper"><a id="bMenu"><span></span></a></div>
 	<ul>
 		<li><a id="logo" href="index.php">Mynaroch</a></li>
 		<li><a href="journal.php">Журнал</a></li>
 		<li><a href="problems.php">Проблемы</a></li>
 		<li><a href="gallery.php">Галерея</a></li>
-		<li id="bWrapper"><a id="bMenu"><span></span></a></li>
 	</ul>
 </menu>
